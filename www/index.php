21df786c0a50e9f0e0e502d263367aceb45520f6
alyabevZakxar <72929720+alyabevZakxar@users.noreply.github.com>
2020-10-15 17:23:03 +0300

    файлы первого макета
    Первый макет

---
+++ www/index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>www</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="section">
+		<h1 class="header">
+			<span>
+			Хочешь заработать миллион?
+		    </span>
+		    <strong>
+		    	открой новый мир вместе с нами!
+		    </strong>
+		</h1>
+
+
+	    <div class="main">
+	 	    <div class="photo">
+	 	    	<img src="photo2.jpg" width="400" height="292">
+	 	    </div>
+	 	    <div class="form">
+	 	    	<p class="form-header">Введите ваши данные!</p>
+	 	    	<form action="#">
+	 	    		<input type="text" name="name" placeholder="Введите имя:">
+	 	    		<input type="text" name="email" placeholder="Введите email:">
+	 	    		<button title="Получить инструкции">
+	 	    			Получить инструкции
+	 	    		</button>
+	 	    	</form>
+	 	    </div>
+	    </div>
+	</div>
+<div class="section">
+	<h2 style="text-transform: uppercase; margin-top: 30px;
+	margin-bottom: 30px; font-size: 18px; font-weight: 600;
+">Почему нужно сделать это сейчас?</h2>
+	<div class="feature" style="text-align: center; ">
+
+	    <div class="col" style="display: inline-block; padding-right: 25px; font-size: 15px; font-weight: bold;">
+		<img src="chip.svg" width="64" height="64">
+		<p>Первое<br> преимущество </p>
+	    </div>
+
+	     <div class="col" style= "display: inline-block; padding-right: 25px; font-size: 15px; font-weight: bold;">
+		<img src="chip.svg" width="64" height="64">
+		<p>Второе <br>преимущество </p>
+	    </div>
+
+	     <div class="col" style= "display: inline-block; padding-right: 25px; font-size: 15px; font-weight: bold; ">
+		<img src="chip.svg" width="64" height="64">
+		<p>Третье <br>преимущество</p>
+	    </div>
+
+	     <div class="col" style= "display: inline-block; font-size: 15px; font-size: 15px;  font-weight: bold;">
+		<img src="chip.svg" width="64" height="64">
+		<p ">Четвёртое<br>  преимущество</p>
+
+	    </div>
+	    <br>
+        <button title="Получить инструкции" style="
+	font-size: 18px;
+	font-weight: 900;
+	color: #fff;
+	background-color: #2727d9;
+	border: none;
+	width: 300px;
+	height: 60px;
+	margin-top: 50px;
+
+	;
+">
+	 	    			Получить инструкции
+	 	    		</button>
+    </div> 
+</div>
+
+</body>
+</html>
